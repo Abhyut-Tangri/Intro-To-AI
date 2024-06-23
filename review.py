0dc39ccc874b8d7671fb118924e91ea00215b549
Abhyut-Tangri <79888918+Abhyut-Tangri@users.noreply.github.com>
2024-06-23 07:54:34 -0700

    Add files via upload

---
+++ review.py
@@ -0,0 +1,34 @@
+#PRACTICING SIMPLE FUNCTIONS AND REVIEW]
+#basic operations:
+def add(x,y):
+    return x+y
+
+
+def subtract(x,y):
+    return x-y
+
+
+def mult(x,y):
+    return x*y
+
+
+def div(x,y):
+    return x/y
+
+#basic data manipulations
+
+
+#lists:
+def find_max(a_lst):
+    return max(a_lst)
+
+
+def find_min(a_lst):
+    return min(a_lst)
+
+
+def total_obj(a_lst):
+    return len(a_lst)
+
+    
+
